c3cb5e990714f4fd816c81bebd4f156d70c2c67a
Allie Cat <allisonemilycoleman@gmail.com>
2025-09-22 12:35:46 -0700

    feat: fix game cards with images on home page and add title header. hopefully fix icon inconsistencies in prod

---
+++ app/components/GameCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <UCard class="flex flex-col">
+    <template #header>
+      <img :src="game.image" :alt="game.title" class="w-full h-48 object-cover mb-4 rounded-md" />
+      <h3 class="text-xl font-bold">{{ game.title }}</h3>
+    </template>
+    <p class="text-gray-500 dark:text-gray-400 flex-grow">{{ game.description }}</p>
+    <template #footer>
+      <NuxtLink :to="`/games/${game.slug}`" class="text-primary-500 hover:underline">
+        Learn More
+      </NuxtLink>
+    </template>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  game: {
+    title: string;
+    description: string;
+    image: string;
+    slug: string;
+  };
+}>();
+</script>
